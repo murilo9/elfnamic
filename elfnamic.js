b2a201aa26766ed3ca92370ea8b4c5dec8a1f8f9
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2019-01-18 14:59:36 -0200

    Propriedades dos elementos aparecem no painel de propriedades

--- elfnamic.js
+++ elfnamic.js
@@ -1,12 +1,22 @@
 var virtualDocument = document.getElementById('document');
 var selectedElement = virtualDocument;    //O default é o document
+var editPanel = document.getElementById("tool-panel");
+
+var divStyle = "display: auto; color: black; background: none; padding: 10px; "+
+"border: 1px solid black; margin: 0px";
+var spanStyle = "display: auto; color: black; background: none; padding: 10px; "+
+"border: 1px dotted black; margin: 0px";
+var pStyle = spanStyle;
+var hStyle = "display: auto; color: black; background: none; padding: 0px; border: node;"
+var buttonStyle = "display: auto; color: black; background: gray; padding: 6px; "+
+"border: 1px solid black; margin: 0px";
 
 function insertDiv(element){
     //Verifica se o elemento pai é uma div ou li:
     var parent = element.parentElement.nodeName;
     if(parent == 'BODY' || parent == 'DIV' || parent == 'LI'){
         //Gera a div a ser inserida
-        element.innerHTML += "<div style='padding: 10px; border: 1px solid gray' onclick='selectThis(this)'><div>";
+        element.innerHTML += "<div style='"+divStyle+"' onclick='selectThis(this)'><div>";
     }else
         alert('Divs só podem ser inseridas em listas ou outras divs');
 }
@@ -16,7 +26,7 @@ function insertSpan(element){
     var parent = element.parentElement.nodeName;
     if(parent != 'INPUT'){
         //Gera a span a ser inserida
-        element.innerHTML += "<span style='border: 1px dotted black' onclick='selectThis(this)'>span</span>"
+        element.innerHTML += "<span style='"+spanStyle+"' onclick='selectThis(this)'>span</span>"
     }else
         alert('Spans não podem ser inseridos em inputs');
 }
@@ -26,7 +36,7 @@ function insertP(element){
     var parent = element.parentElement.nodeName;
     if(parent != 'BUTTON' || parent != 'INPUT'){
         //Gera o parágrafo a ser inserido:
-        element.innerHTML += "<p style='border: 1px dotted black' onclick='selectThis(this)'>Parágrafo</p>"
+        element.innerHTML += "<p style='"+pStyle+"' onclick='selectThis(this)'>Parágrafo</p>"
     }else
         alert('Parágrafos não podem ser inseridos em buttons ou inputs');
 }
@@ -48,7 +58,7 @@ function insertHeader(element){
         //VPega o tamanho de título que foi selecionado:
         var selectedHeader = document.getElementById('selectHeader').value;
         //Gera o título a ser inserido:
-        element.innerHTML += "<"+selectedHeader+" onclick='selectThis(this)'>Título</"+selectedHeader+">";
+        element.innerHTML += "<"+selectedHeader+" onclick='selectThis(this)' style='"+hStyle+"'>Título</"+selectedHeader+">";
     }else
         alert('Títulos não podem ser inseridos em buttons ou inputs');
 }
@@ -59,7 +69,7 @@ function insertButton(element){
     if(parent == 'BODY' || parent == 'LI' || parent == 'DIV' || parent == 'SPAN' ||
         parent == 'TD' || parent == 'TH' || parent == 'LI'){
         //Gera o button a ser inserido:
-        element.innerHTML += "<button type='button' onclick='selectThis(this)'>Botão</button>";
+        element.innerHTML += "<button type='button' style='"+buttonStyle+"' onclick='selectThis(this)'>Botão</button>";
     }else
         alert('Buttons só podem ser inseridos em: div, span, td, th ou li');
     
@@ -70,7 +80,7 @@ function insertButton(element){
 */
 function deleteThis(element){
     if(element != virtualDocument)      //Se o objeto selecionado não for o virtualDocument
-        element.parentNode.removeChild(element);    //Acesso o parent node e remove este elemento dele
+        element.parentNode.removeChild(element);    //Acessa o parent node e remove este elemento dele
     selectedElement = virtualDocument;      //Deixa o virtualDocument selecionado, já que seleção fica nula
 }
 
@@ -83,4 +93,22 @@ function selectThis(element){
     if(element != virtualDocument)      //Se o clique foi em algum elemento (e não pra deselecionar)
         element.style.opacity = 0.3;            //Ajusta a opacity do elemento pra 0.3
     selectedElement = element;          //Enfim, deixa o elemento selecionado
+    showData(element);      //Chama a função de exibir os dados no painel
+}
+
+/*
+    Recebe uma referência ao elemento e exibe os dados dele no painel de propriedades
+*/
+function showData(element){
+    //TODO fazer com que apareça um painel diferente para cada tipo de elemento
+    $('#propDisplay').val(element.style.display);
+    $('#propColor').val(element.style.color);
+    $('#propBkg').val(element.style.background);
+    $('#propMargin').val(element.style.margin);
+    $('#propBorder').val(element.style.border);
+    $('#propPadding').val(element.style.padding);
+}
+
+function aplicarEstilo(){
+    alert('Aplicar estilo');
 }
